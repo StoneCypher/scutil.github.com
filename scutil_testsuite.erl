4fc488c269c2c9a72505d5756c6e77b1ddbbafff
John Haugeland <stonecypher@gmail.com>
2008-07-25 03:07:19 +0000

    Finished type_of() testsuite

--- scutil_testsuite.erl
+++ scutil_testsuite.erl
@@ -31,26 +31,19 @@
 
 test_type_of() ->
 
-    [   testerl:must_equal("integer type_of()",  fun scutil:type_of/1, [1],               integer),
-        testerl:must_equal("float type_of()",     fun scutil:type_of/1, ["1"],             list),
-        testerl:must_equal("list type_of()",     fun scutil:type_of/1, ["1"],             list),
-        testerl:must_equal("binary type_of()",   fun scutil:type_of/1, [<<1>>],           binary),
-        testerl:must_equal("function type_of()", fun scutil:type_of/1, [fun(X) -> X end], function)
+    [APort | _Discard] = erlang:ports(),
 
-type_of(X) when is_integer(X)   -> integer;
-type_of(X) when is_float(X)     -> float;
-type_of(X) when is_number(X)    -> number;
-type_of(X) when is_list(X)      -> list;
-type_of(X) when is_tuple(X)     -> tuple;
-%type_of(X) when is_bitstring(X) -> bitstring;  % will fail before e12
-type_of(X) when is_binary(X)    -> binary;     % redundant in e12
-type_of(X) when is_boolean(X)   -> boolean;
-type_of(X) when is_function(X)  -> function;
-type_of(X) when is_pid(X)       -> pid;
-type_of(X) when is_port(X)      -> port;
-type_of(X) when is_reference(X) -> reference;
-type_of(X) when is_atom(X)      -> atom;
-
+    [   testerl:must_equal("integer type_of()",   fun scutil:type_of/1, [1],               integer),
+        testerl:must_equal("float type_of()",     fun scutil:type_of/1, [1.1],             float),
+        testerl:must_equal("list type_of()",      fun scutil:type_of/1, ["1"],             list),
+        testerl:must_equal("tuple type_of()",     fun scutil:type_of/1, [{1}],             tuple),
+        testerl:must_equal("binary type_of()",    fun scutil:type_of/1, [<<1>>],           binary),
+        testerl:must_equal("boolean type_of()",   fun scutil:type_of/1, [true],            boolean),
+        testerl:must_equal("function type_of()",  fun scutil:type_of/1, [fun(X) -> X end], function),
+        testerl:must_equal("pid type_of()",       fun scutil:type_of/1, [self()],          pid),
+        testerl:must_equal("port type_of()",      fun scutil:type_of/1, [APort],           port),
+        testerl:must_equal("reference type_of()", fun scutil:type_of/1, [make_ref()],      reference),
+        testerl:must_equal("atom type_of()",      fun scutil:type_of/1, [lol],             atom)
     ].
 
 
