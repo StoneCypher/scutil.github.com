3e5d799e7b36a0ecf5408bb3c79f3251a6a96d5f
John Haugeland <stonecypher@gmail.com>
2009-02-21 22:31:44 +0000

    Added flesch_kincaid_readability/4, flesch_kincaid_readability_score/3, interpret_flesch_kincaid_readability_score/1

--- scutil.erl
+++ scutil.erl
@@ -3401,3 +3401,48 @@ module_atoms(Module)         -> beam_lib:chunks(Module, [atoms]).
 
 %% @since Version 130
 module_abstract_code(Module) -> beam_lib:chunks(Module, [atoms]).
+
+
+
+
+
+% http://en.wikipedia.org/wiki/Flesch-Kincaid_Readability_Test
+
+%% @since Version 131
+
+flesch_kincaid_readability(Data, WordCounter, SentenceCounter, SyllableCounter) ->
+
+    Words     = WordCounter(Data),
+    Sentences = SentenceCounter(Data),
+    Syllables = SyllableCounter(Data),
+
+    interpret_flesch_kincaid_readability_score(
+      flesch_kincaid_readability_score(Words, Sentences, Syllables)
+    ).
+
+
+
+
+
+%% @since Version 131
+
+interpret_flesch_kincaid_readability_score(R) when R > 100 -> { easy_before_11_years,     R };
+interpret_flesch_kincaid_readability_score(R) when R >  90 -> { easy_at_11_years,         R };
+interpret_flesch_kincaid_readability_score(R) when R >  70 -> { easy_for_11_to_13_years,  R };
+interpret_flesch_kincaid_readability_score(R) when R >  60 -> { easy_for_13_to_15_years,  R };
+interpret_flesch_kincaid_readability_score(R) when R >  30 -> { appropriate_for_15_years, R };
+interpret_flesch_kincaid_readability_score(R) when R >   0 -> { appropriate_for_college,  R };
+interpret_flesch_kincaid_readability_score(R)              -> { difficult,                R }.
+
+
+
+
+
+% http://en.wikipedia.org/wiki/Flesch-Kincaid_Readability_Test
+% http://www.readabilityformulas.com/graphics/fleschresults.gif
+
+%% @since Version 131
+
+flesch_kincaid_readability_score(Words, Sentences, Syllables) ->
+
+    206.835 - (1.015 * (Words/Sentences)) - (84.6 * (Syllables/Words)).
