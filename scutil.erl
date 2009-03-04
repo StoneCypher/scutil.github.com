59b1695ab09fa791d4ba1593fa28ac3d03975372
John Haugeland <stonecypher@gmail.com>
2009-03-04 09:41:59 +0000

    Whoops.  DRY on eval().

--- scutil.erl
+++ scutil.erl
@@ -3581,14 +3581,16 @@ halstead_complexity(DistinctOperators, DistinctOperands, TotalOperators, TotalOp
 %% @since Version 146
 % modified from http://www.trapexit.org/String_Eval
 
-eval(S) ->
-    {ok,Scanned,_} = erl_scan:string(S),
-    {ok,Parsed} = erl_parse:parse_exprs(Scanned),
-    erl_eval:exprs(Parsed,erl_eval:new_bindings()).
+eval(S) -> eval(S,erl_eval:new_bindings()).
+
+
+
+
 
 %% @since Version 146
 % from http://www.trapexit.org/String_Eval
 eval(S, Environ) ->
+
     {ok,Scanned,_} = erl_scan:string(S),
     {ok,Parsed} = erl_parse:parse_exprs(Scanned),
     erl_eval:exprs(Parsed,Environ).
