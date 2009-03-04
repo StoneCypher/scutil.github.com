3345296353424ed01367680d0644911a1cbaed1f
John Haugeland <stonecypher@gmail.com>
2009-03-04 09:32:34 +0000

    Updated gen_docs to handle init stop timing issue

--- scutil.erl
+++ scutil.erl
@@ -3599,5 +3599,6 @@ eval(S, Environ) ->
 
 gen_docs([From, To]) -> 
     
-    edoc:files( [From++"scutil/scutil.erl", From++"scutil/dq.erl"], [{dir, To}]).
+    edoc:files( [From++"scutil/scutil.erl", From++"scutil/dq.erl"], [{dir, To}]),
+    receive after 20000 -> init:stop().
 
