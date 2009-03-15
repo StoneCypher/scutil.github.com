b3311379dcaae77e1857823c9fd8e536e6abfa89
John Haugeland <stonecypher@gmail.com>
2009-03-15 21:32:52 +0000

    Finally got better EDoc working <3

--- src/scutil.erl
+++ src/scutil.erl
@@ -3642,7 +3642,7 @@ eval(S, Environ) ->
 %% @since Version 152
 gen_docs([From, To]) ->
 
-    edoc:files( [ From ++ "/src/" ++ X ++ ".erl" || X <- ["scutil","dq"] ] , [{dir, To}]).
+    edoc:application(scutil, From, [{dir,To},{new,true}] ).
 
 
 
