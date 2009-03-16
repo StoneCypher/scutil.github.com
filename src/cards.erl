61e8bbbdcd158b3a05271da2d3f8f82e594fee29
John Haugeland <stonecypher@gmail.com>
2009-03-16 01:49:25 +0000

    Added scutil:columnate/1,2 Some edoc on cards module

--- src/cards.erl
+++ src/cards.erl
@@ -47,28 +47,72 @@
 
 
 
+%% @spec mameluke_deck() -> list()
+%% @doc One of the oldest decks of cards known, originating from Egypt; probably the point between Chinese Money Cards and the early European decks. 
+%% ```1> scutil:columnate(cards:mameluke_deck(), [{columns,4}]).
+%% ["   {1,polo_stick}         {1,coin}         {1,sword}         {1,cup}   ",
+%%  "   {2,polo_stick}         {2,coin}         {2,sword}         {2,cup}   ",
+%%  "   {3,polo_stick}         {3,coin}         {3,sword}         {3,cup}   ",
+%%  "   {4,polo_stick}         {4,coin}         {4,sword}         {4,cup}   ",
+%%  "   {5,polo_stick}         {5,coin}         {5,sword}         {5,cup}   ",
+%%  "   {6,polo_stick}         {6,coin}         {6,sword}         {6,cup}   ",
+%%  "   {7,polo_stick}         {7,coin}         {7,sword}         {7,cup}   ",
+%%  "   {8,polo_stick}         {8,coin}         {8,sword}         {8,cup}   ",
+%%  "   {9,polo_stick}         {9,coin}         {9,sword}         {9,cup}   ",
+%%  "  {10,polo_stick}        {10,coin}        {10,sword}        {10,cup}   ",
+%%  " {duke,polo_stick}      {duke,coin}      {duke,sword}      {duke,cup}  ",
+%%  "{viceroy,polo_stick}   {viceroy,coin}   {viceroy,sword}   {viceroy,cup}",
+%%  " {king,polo_stick}      {king,coin}      {king,sword}      {king,cup}  "]'''
+%% @since Version 200
 mameluke_deck()      -> [ {Face,Suit} || Face <- [1,2,3,4,5,6,7,8,9,10,duke,viceroy,king], Suit <- [polo_stick,coin,sword,cup] ].
+
+%% @since Version 200
 french_deck()        -> [ {Face,Suit} || Face <- [ace,2,3,4,5,6,7,8,9,10,knave,queen,king], Suit <- [hearts,spades,diamonds,clubs] ].
+
+%% @since Version 200
 american_deck()      -> [ {Face,Suit} || Face <- [ace,2,3,4,5,6,7,8,9,10,jack,queen,king], Suit <- [hearts,spades,diamonds,clubs] ] ++ [ {joker,big}, {joker,little} ].
+
+%% @since Version 200
 german_deck()        -> [ {Face,Suit} || Face <- [ace,2,3,4,5,6,7,8,9,10,jack,queen,king], Suit <- [hearts,bells,leaves,acorns] ] ++ [ {joker,big}, {joker,little} ].
+
+%% @since Version 200
 domino_deck()        -> [ {A,B} || A <- [1,2,3,4,5,6], B <- [1,2,3,4,5,6], A =< B ].
+
+%% @since Version 200
 chinese_money_deck() -> [ {Val,Suit} || Val <- [2,3,4,5,6,7,8,9], Suit <- [coins,strings,myriads,tens_of_myriads] ] ++ [{1,tens_of_myriads}].   % that's right, 2-9 in each suit except tens, which is 1-9.
+
+%% @since Version 200
 piquet_deck()        -> [ {Face,Suit} || Face <- [ace,7,8,9,10,jack,queen,king], Suit <- [hearts,spades,diamonds,clubs] ].
+
+%% @since Version 200
 pinochle_deck()      -> [ {Face,Suit} || Face <- [ace,9,10,jack,queen,king], Suit <- [hearts,spades,diamonds,clubs] ].
 
+%% @since Version 200
 tarot_deck()         -> [ {Face,Suit} || Face <- [ace,2,3,4,5,6,7,8,9,10,jack,knight,queen,king], Suit <- [hearts,spades,diamonds,clubs] ] ++ [ {major,X} || X <- lists:seq(1,21) ] ++ [{the_fool,0}].
 
+%% @since Version 200
 doppelkopf_deck()         -> doppelkopf_deck(nines).
+
+%% @since Version 200
 doppelkopf_deck(nines)    -> [ {Face,Suit} || Face <- [ace,9,10,jack,queen,king], Suit <- [hearts,bells,leaves,acorns] ];
 doppelkopf_deck(no_nines) -> [ {Face,Suit} || Face <- [ace,10,jack,queen,king], Suit <- [hearts,bells,leaves,acorns] ].
 
+%% @since Version 200
 hungarian_deck() -> [ {Face,Suit} || Face <-    [vii,viii,ix,x,under,over,king,ace], Suit <- [hearts,bells,leaves,acorns] ].
+
+%% @since Version 200
 austrian_deck()  -> [ {Face,Suit} || Face <- [vi,vii,viii,ix,x,under,over,king,ace], Suit <- [hearts,bells,leaves,acorns] ].
+
+%% @since Version 200
 swiss_deck()     -> [ {Face,Suit} || Face <- [6,7,8,9,banner,under,over,king,ace], Suit <- [roses,bells,acorns,shields] ].
+
+%% @since Version 200
 italian_deck()   -> [ {Face,Suit} || Face <- [ace,2,3,4,5,6,7,infantry,cavalry,king], Suit <- [coins,swords,cups,clubs] ].
 
+%% @since Version 200
 euchre_deck()    -> [ {Face,Suit} || Face <- [9,10,jack,queen,king,ace], Suit <- [hearts,spades,diamonds,clubs] ].
 
+%% @since Version 200
 mille_bornes_deck() -> scutil:key_duplicate([
   {3,{hazard,accident}},{3,{hazard,out_of_gas}},{3,{hazard,flat_tire}},{4,{hazard,speed_limit}},{5,{hazard,stop}},
   {6,{remedy,repair}},{6,{remedy,gasoline}},{6,{remedy,spare_tire}},{6,{remedy,end_of_speed_limit}},{14,{remedy,go}},
@@ -76,4 +120,5 @@ mille_bornes_deck() -> scutil:key_duplicate([
   {10,{distance,25}},{10,{distance,50}},{10,{distance,75}},{12,{distance,100}},{4,{distance,200}}
 ]).
 
+%% @since Version 200
 uno_deck() -> [ {Face,Color} || Face <- lists:seq(0,9)++lists:seq(1,9)++[reverse,reverse,skip,skip,draw_two,draw_two], Color <- [red,yellow,green,blue] ] ++ lists:flatten(lists:duplicate(4, [{draw_four,wild},{wild,wild}])).
--- src/scutil.erl
+++ src/scutil.erl
@@ -436,7 +436,8 @@
 
     list_rotate/2, index_of_first/2, rotate_to_first/2, rotate_first_to_end/2, % needs tests
 
-    columnated_rows/2, columns/2, % needs tests
+    columnated_rows/2, columns/2, columnate/1, columnate/2, % needs tests
+    first_row/1, first_or_nothing/1, % needs tests
     
     floor/1, ceil/1, ceiling/1, % needs tests
     
@@ -3723,10 +3724,10 @@ rotate_first_to_end(Item, List) ->
 
 
 %% @since Version 171
-columnated_rows(RowCount, List) -> columnated_rows(List, lists:duplicate(RowCount, []), 0).
+columns(RowCount, List) -> columns(List, lists:duplicate(RowCount, []), 0).
 
-columnated_rows([],             Output,          Unrotate) -> list_rotate(Unrotate, [ lists:reverse(Column) || Column <- Output]);
-columnated_rows([Item|ListRem], [Output|OutRem], Unrotate) -> columnated_rows(ListRem, OutRem++[[Item]++Output], Unrotate-1).
+columns([],             Output,          Unrotate) -> list_rotate(Unrotate, [ lists:reverse(Column) || Column <- Output]);
+columns([Item|ListRem], [Output|OutRem], Unrotate) -> columns(ListRem, OutRem++[[Item]++Output], Unrotate-1).
 
 
 
@@ -3761,9 +3762,71 @@ ceiling_t(T,_Td)             -> T.
 
 
 %% @since Version 173
-columns(ColumnCount, List) ->
+columnated_rows(ColumnCount, List) ->
 
-    columnated_rows(ceiling(length(List) / ColumnCount), List).
+    columns(ceiling(length(List) / ColumnCount), List).
+
+
+
+
+
+%% @since Version 204
+first_or_nothing([H|T]) -> {H,T};
+first_or_nothing([])    -> {[],[]}.
+
+first_row(Columns) -> first_row(Columns, [], []).
+
+first_row([],                OutCols, Work) -> { lists:reverse(Work), lists:reverse(OutCols) };
+first_row([ThisCol|RemCols], OutCols, Work) ->
+    {Item,ColRem} = first_or_nothing(ThisCol),
+    first_row(RemCols, [ColRem]++OutCols, [Item]++Work).
+
+
+
+
+
+%% @equiv columnate(List, 2, 3)
+%% @since Version 204
+columnate(List) -> columnate(List, []).
+
+%% @since Version 204
+columnate(List, Options) ->
+
+    Settings = lists:keymerge(1, Options, [{columns, 2}, {margin, 3}, {align, center}] ),
+
+    [ColumnCount, Margin, Align] = [proplists:get_value(X, Settings) || X <- [columns,margin,align] ],
+
+    Columns = scutil:columns(ColumnCount, List),
+
+    MinWidths = [ lists:max([length(lists:flatten(io_lib:format("~w",[Item]))) || Item <- Col]) || Col <- Columns ],
+
+    DoAlign = fun(Item, Width) ->
+        case Align of
+            left   -> string:left(  lists:flatten(io_lib:format("~w",[Item])), Width);
+            center -> string:centre(lists:flatten(io_lib:format("~w",[Item])), Width);
+            centre -> string:centre(lists:flatten(io_lib:format("~w",[Item])), Width);
+            right  -> string:right( lists:flatten(io_lib:format("~w",[Item])), Width)
+        end
+    end,
+
+    Aligned = [ [ DoAlign(Item, Width) || Item <- Col]  || {Width, Col} <- lists:zip(MinWidths, Columns) ],
+
+    Format = implode(lists:duplicate(Margin, $ ), ["~"++integer_to_list(Width)++"s"||Width<-MinWidths]),
+
+    columnate_each_row(Aligned, Format, []).
+
+
+
+
+
+%% @since Version 204
+columnate_each_row([ []|_],_Format, Output) -> lists:reverse(Output);
+columnate_each_row(Columns, Format, Output) ->
+
+    {ThisRow, RemRows} = first_row(Columns),
+    ThisOut = lists:flatten(io_lib:format(Format, ThisRow)),
+    columnate_each_row(RemRows, Format, [ThisOut]++Output).
+
 
 
 
