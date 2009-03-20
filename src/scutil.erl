12b35d90414886c49cfcae9d80ad6f1d36d1ca40
John Haugeland <stonecypher@gmail.com>
2009-03-20 22:27:39 +0000

    Added expected_value/1

--- src/scutil.erl
+++ src/scutil.erl
@@ -459,10 +459,12 @@
     is_numeric_string/1, is_numeric_string/2, % needs tests
     
     explode/2, explode/3, % needs tests
-    
+
     starts_with/2, % needs tests
+
+    extrema_of/1, % needs tests
     
-    extrema_of/1 % needs tests
+    expected_value/1
 
 ] ).
 
@@ -5540,3 +5542,23 @@ extrema_of(List) ->
         {First,First},
         List
     ).
+
+
+
+
+
+%% @spec expected_value(List::mixed_weight_list()) -> number()
+
+%% @doc {@section Probability} Returns the expected value of infinite selection from a weighted numeric list.  ```1> scutil:expected_value([1,2,3,4,5,6]).
+%% 3.50000
+%%
+%% 2> scutil:expected_value([ {-1,37}, {35,1} ]).
+%% -5.26316e-2'''
+
+%% @since Version 222
+
+expected_value(List) -> expected_value(List, 0, 0).
+
+expected_value( []                                , Sum, Range) -> Sum/Range;
+expected_value( [ {Value,Probability} | Remainder], Sum, Range) -> expected_value(Remainder, Sum+(Value*Probability), Range+Probability);
+expected_value( [ UnweightedItem      | Remainder], Sum, Range) -> expected_value([{UnweightedItem,1}] ++ Remainder, Sum, Range).
