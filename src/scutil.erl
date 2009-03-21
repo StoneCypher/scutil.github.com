b415a2cb255afc484cda8d15116223fbd95a0d95
John Haugeland <stonecypher@gmail.com>
2009-03-21 04:41:51 +0000

    Rename rename rename HAY

--- src/scutil.erl
+++ src/scutil.erl
@@ -4668,65 +4668,6 @@ flesch_kincaid_readability_score(Words, Sentences, Syllables) ->
 
 
 
-%% @since Version 139
-
-halstead_complexity(DistinctOperators, DistinctOperands, TotalOperators, TotalOperands) ->
-
-    halstead_complexity(DistinctOperators, DistinctOperands, TotalOperators, TotalOperands, brief).
-
-
-
-%% @since Version 139
-
-halstead_complexity(DistinctOperators, DistinctOperands, TotalOperators, TotalOperands, brief) ->
-
-    { Effort, _ } = halstead_complexity(DistinctOperators, DistinctOperands, TotalOperators, TotalOperands, complete),
-    Effort;
-
-
-
-%% @since Version 139
-
-halstead_complexity(DistinctOperators, DistinctOperands, TotalOperators, TotalOperands, complete) ->
-
-    ProgramLength     = TotalOperators    + TotalOperands,
-    ProgramVocabulary = DistinctOperators + DistinctOperands,
-
-    Volume            = ProgramLength         * (math:log(ProgramVocabulary)),
-    Difficulty        = (DistinctOperators/2) * (TotalOperands/DistinctOperands),
-
-    Effort            = Volume * Difficulty,
-
-    { Effort, [{volume, Volume}, {difficulty, Difficulty}, {program_length, ProgramLength}, {program_vocabulary, ProgramVocabulary}] }.
-
-
-
-
-
-%% @since Version 146
-% modified from http://www.trapexit.org/String_Eval
-
-eval(S) -> 
-
-    eval(S,erl_eval:new_bindings()).
-
-
-
-
-
-%% @since Version 146
-% from http://www.trapexit.org/String_Eval
-
-eval(S, Environ) ->
-
-    {ok, Scanned,_} = erl_scan:string(S),
-    {ok, Parsed}    = erl_parse:parse_exprs(Scanned),
-    erl_eval:exprs(Parsed,Environ).
-
-
-
-
-
 %% @since Version 152
 
 gen_docs( [From, To] ) ->
