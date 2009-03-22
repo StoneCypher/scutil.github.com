e16e57513c5f2aa8efc696844b48ed5c3c717c7b
John Haugeland <stonecypher@gmail.com>
2009-03-22 21:56:55 +0000

    Added more stray modules

---
+++ src/sc/htstub.erl
@@ -0,0 +1,23 @@
+
+%% @author John Haugeland <stonecypher@gmail.com>
+%% @copyright 2007 - current John Haugeland, All Rights Reserved
+%% @version $Revision$
+%% @since Version 8
+
+%% @doc <!-- google analytics --><script type="text/javascript">var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));</script><script type="text/javascript">var pageTracker = _gat._getTracker("UA-4903191-10");pageTracker._trackPageview();</script>
+%% <p></p>
+
+
+
+%% @reference <span style="padding:0.1em 0.4em;background-color:#eef;display:inline-block;width:47em"><span style="display:inline-block;width:18em">Website is</span><a href="http://scutil.com/">http://scutil.com/</a></span>
+%% @reference <span style="padding:0.1em 0.4em;background-color:#efe;display:inline-block;width:47em"><span style="display:inline-block;width:18em">Author's Website</span><a href="http://fullof.bs">Full of BS</a></span>
+%% @reference <span style="padding:0.1em 0.4em;background-color:#eef;display:inline-block;width:47em"><span style="display:inline-block;width:18em">This library is released under the</span><a href="http://scutil.com/license.html">MIT License</a></span>
+%% @reference <span style="padding:0.1em 0.4em;background-color:#efe;display:inline-block;width:47em"><span style="display:inline-block;width:18em">This build was released</span><tt style="text-decoration:underline;background-color:#eee">$Date: 2009-03-15 13:47:01 -0600 (Sun, 15 Mar 2009) $</tt></span>
+
+%% @todo add @see cross-references between related functions
+%% @todo add thanks tables and cross-references
+%% @todo add dependant libraries table
+%% @todo add untested warnings to beginnings of @doc tags
+%% @todo add defective warnings to beginnings of @doc tags
+%% @todo add links to test data
+%% @todo add sections to examples: descriptive text, code example, what's it for, related, thanks
