b5370bcd9d0cf4237227ad6e5a693a9beabcca32
John Haugeland <stonecypher@gmail.com>
2009-03-30 21:29:54 +0000

    Adding PHP utilities <3

---
+++ php/ScPhpUtil.php
@@ -0,0 +1,21 @@
+<?php
+
+  // $Revision: 7 $
+
+  // can't use class wrapped version enforcement in case the class breaks from too-old php before it gets there, lol
+  // always use the class method version instead plskthx
+
+  function EnforceMinimumPhpVersion($ver, $message)
+
+      if (!(version_compare(phpversion(), $ver) === 1)) {
+
+          die (
+          );
+
+      }
+
+  }
+
+  EnforceMinimumPhpVersion('5.1.6', '<html><head><title>Needs newer PHP</title></head><body><p>Requires a minimum PHP version of <tt>5.1.6</tt> .</p></body></html>');
+
+?>
