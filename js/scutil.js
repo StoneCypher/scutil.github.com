bb428376dabcf44cf3f7f55d5e3e484322bc5901
John Haugeland <stonecypher@gmail.com>
2009-03-31 19:32:44 +0000

    Added scutil.js:RemoveClassOnceIfPresent/2

--- js/scutil.js
+++ js/scutil.js
@@ -12,3 +12,20 @@ function AddClassIfMissing(Tag, tclass) {
       Tag.className += (' ' + tclass);
   }
 }
+
+
+
+
+
+// only removes the first instance of the class if repeated
+
+function RemoveClassOnceIfPresent(Tag, tclass) {
+  var Classes = Tag.className.split(' ');
+  for (i in Classes) {
+    if (Classes[i] == tclass) {
+      Classes.splice(i, 1);
+      Tag.className = Classes.join(' ');
+      return;
+    }
+  }
+}
