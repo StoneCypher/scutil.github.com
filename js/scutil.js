61b48fd93a921ebc71239c164acbb98c58f2e4f6
John Haugeland <stonecypher@gmail.com>
2009-03-31 19:33:19 +0000

    Added scutil.js:RemoveClassRepeatedlyIfPresent/2

--- js/scutil.js
+++ js/scutil.js
@@ -29,3 +29,17 @@ function RemoveClassOnceIfPresent(Tag, tclass) {
     }
   }
 }
+
+
+
+
+
+function RemoveClassRepeatedlyIfPresent(Tag, tclass) {
+  var Classes = Tag.className.split(' ');
+  for (i in Classes) {
+    if (Classes[i] == tclass) {
+      Classes.splice(i, 1);
+      Tag.className = Classes.join(' ');
+    }
+  }
+}
