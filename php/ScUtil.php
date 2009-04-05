a7f1d4d9270c1d7d20ef7c20c088ada4511bbfc4
John Haugeland <stonecypher@gmail.com>
2009-04-05 23:23:01 +0000

    Added ScUtil.php:rgb_to_hsv/3 Whoops, fixed properties after rename Minor reformatting

--- php/ScUtil.php
+++ php/ScUtil.php
@@ -1,6 +1,14 @@
 <?php
 
-  // $Revision: 317 $
+  // $Revision$
+  // Since Apr 2, 2009
+  // $Date$
+  // $HeadURL$
+  // $Id$
+
+
+
+
 
   require_once('ScPhpTest.php');
 
@@ -139,6 +147,46 @@
               's' => $S,
               'v' => $V
           );
+
+      }
+
+
+
+
+
+      function hsv_to_rgb($H, $S, $V) { // HSV Values:Number 0-1, RGB Results:Number 0-255
+
+          $RGB = array();
+
+          if ($S == 0) {
+
+              $R =
+              $G =
+              $B = $V * 255;
+
+          } else {
+
+              $var_H = $H * 6;
+              $var_i = floor( $var_H );
+
+              $var_1 = $V * ( 1 - $S );
+              $var_2 = $V * ( 1 - $S *      ( $var_H - $var_i )   );
+              $var_3 = $V * ( 1 - $S * (1 - ( $var_H - $var_i ) ) );
+
+              if      ($var_i == 0) { $vR = $V;     $vG = $var_3; $vB = $var_1; }
+              else if ($var_i == 1) { $vR = $var_2; $vG = $V;     $vB = $var_1; }
+              else if ($var_i == 2) { $vR = $var_1; $vG = $V;     $vB = $var_3; }
+              else if ($var_i == 3) { $vR = $var_1; $vG = $var_2; $vB = $V;     }
+              else if ($var_i == 4) { $vR = $var_3; $vG = $var_1; $vB = $V;     }
+              else                  { $vR = $V;     $vG = $var_1; $vB = $var_2; }
+
+              $R = $vR * 255;
+              $G = $vG * 255;
+              $B = $vB * 255;
+
+          }
+
+          return array('r'=>$R, 'g'=>$G, 'b'=>$B);
 
       }
 
--- php/ScUtil_Testset.php
+++ php/ScUtil_Testset.php
@@ -1,6 +1,6 @@
 <?php
 
-  // $Revision: 317 $
+  // $Revision$
 
 
 
