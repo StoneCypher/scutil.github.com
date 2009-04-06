e482aedb50882b7fd303de77fedfddbd9044bad7
John Haugeland <stonecypher@gmail.com>
2009-04-06 00:58:14 +0000

    Started ScTagWriter.php

---
+++ php/ScTagWriter.php
@@ -0,0 +1,71 @@
+<?php
+
+  // $Revision$
+  // Since Apr 2, 2009
+  // $Date$
+  // $HeadURL$
+  // $Id$
+
+  // TODO phpDocumentor
+  // MIT License, see http://scutil.com/license.html
+
+
+
+
+
+  require_once('ScUtil.php');
+
+
+
+
+
+  class ScTagWriter implements ScPhpTestableInterface {
+
+
+
+
+
+      public static function Make($tag, $conts='', $args=array()) {
+
+          $iArgs = ScUtil::ImplodeSkippingEmpty(' ', $args);
+
+          $close = ($conts == '')? ('/>') : ('>');
+          $end   = ($conts == '')? ('')   : ("</$tag>");
+
+          $uArgs = ($iArgs == '')? ('')   : (' ' . implode(' ', $args));
+
+          return "<{$tag}{$uArgs}{$close}{$conts}{$end}";
+
+      }
+
+
+
+
+
+      public static function LabelledInput($id, $value='', $type='') {
+
+          $utype  = (($type=='')?  '' : "type=\"$type\"";
+          $uvalue = (($value=='')? '' : "value=\"$value\"";
+
+          return self::Make('label', self::Make(), array($utype, $uvalue));
+
+        echo "<label for=\"$id\" id=\"{$id}label\"><input name=\"$id\" id=\"$id\"";
+
+        if ($type  != '') { echo " type=\"$type\""; }
+        if ($value != '') { echo " value=\"$value\""; }
+
+        echo "></input> $id</label>";
+
+      }
+
+
+
+
+
+  };
+
+
+
+
+
+?>
