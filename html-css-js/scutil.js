3584520e08a9adaf646d4b9e35b06f9a322c1cdf
John Haugeland <stonecypher@gmail.com>
2009-04-30 20:30:18 +0000

    Added scutil.js:DateToOrdinalDate/3, MonthLength/2, IsLeapYear/1

--- html-css-js/scutil.js
+++ html-css-js/scutil.js
@@ -198,3 +198,59 @@ function Zebrafy(Tag, RowType, ClassList) {
     }
 
 }
+
+
+
+
+
+function MonthLength(Month, Year) {
+
+    switch (Month) {
+        case  1: return 31;
+        case  2: return (IsLeapYear(Year))? 29 : 28;
+        case  3: return 31;
+        case  4: return 30;
+        case  5: return 31;
+        case  6: return 30;
+        case  7: return 31;
+        case  8: return 31;
+        case  9: return 30;
+        case 10: return 31;
+        case 11: return 30;
+        case 12: return 31;
+    }
+
+    return false;
+
+}
+
+
+
+
+
+function IsLeapYear(Year) {
+    return (
+        ((Year % 400) == 0)? true  : (
+        ((Year % 100) == 0)? false : (
+        ((Year % 4)   == 0)? true  :
+                             false
+        ))
+    );
+}
+
+
+
+
+
+function DateToOrdinalDate(Month, Day, Year) {
+
+    var Base = 0;
+    var i;
+
+    for (i=1; i<Month; ++i) {
+        Base += MonthLength(i, Year);
+    }
+
+    return { 'year':Year, 'ord':Base+Day };
+
+}
