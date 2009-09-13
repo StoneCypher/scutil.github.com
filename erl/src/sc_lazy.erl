f68ee59710db46fa0c7428b3d0ca08baa6d43dbb
John Haugeland <stonecypher@gmail.com>
2009-09-13 06:05:49 +0000

    Great Erlang Renaming 2: moved sc.lazy to sc_lazy implemented sc_lazy

--- erl/src/sc_lazy.erl
+++ erl/src/sc_lazy.erl
@@ -26,7 +26,7 @@
 
 
 
--module(sc.lazy).
+-module(sc_lazy).
 
 -author("John Haugeland <stonecypher@gmail.com>").
 -webpage("http://scutil.com/").
@@ -51,4 +51,134 @@
 
 
 -export( [
+
+    eval/2,
+    dump/0,
+    terminate/0,
+
+
+
+
+
+    % private, do not call below this line
+
+    cache_core/0
+
 ] ).
+
+
+
+
+
+%% @since Version 408
+
+cache_core() ->
+
+    {ShouldContinue, Result} = receive
+
+        { Sender, terminate } ->
+            { do_not_continue, { terminating, self() } };
+
+        { Sender, dump } ->
+            { continue, get() };
+
+        { Sender, dump, Fun } ->
+            { continue, [ {{F,A},Val} || {{F,A},Val} <- get(), F == Fun ] };
+
+        { Sender, lazy_apply, Fun, Args } ->
+            case get({Fun,Args}) of
+
+                undefined ->
+                    E = apply(Fun, Args),
+                    put({Fun,Args}, {val, E}),
+                    {continue, E};
+
+                {val,V} ->
+                    {continue, V}
+            end
+
+    end,
+    Sender ! { sc_lazy_cache, Result },
+
+    case ShouldContinue of
+        continue        -> cache_core();
+        do_not_continue -> ok
+    end.
+
+
+
+
+
+%% @since Version 408
+
+terminate() ->
+
+    case whereis(sc_lazy_cache) of
+
+        undefined ->
+            { error, sc_lazy_cache_not_running };
+
+        Defined   ->
+            Defined ! { self(), terminate },
+            receive
+                { sc_lazy_cache, Message } -> { sc_lazy_cache, Message }
+            end
+
+    end.
+
+
+
+
+
+%% @since Version 408
+
+dump() ->
+
+    case whereis(sc_lazy_cache) of
+
+        undefined ->
+            { error, sc_lazy_cache_not_running };
+
+        Defined   ->
+            Defined ! { self(), dump },
+            receive
+                { sc_lazy_cache, Message } -> { sc_lazy_cache, Message }
+            end
+
+    end.
+
+
+
+
+
+%% @since Version 408
+
+% 1> F = fun(X) -> receive after 10000 -> X*2 end end.
+% #Fun<erl_eval.6.13229925>
+%
+% 2> sc_time:benchmark(fun sc_lazy:eval/2, [F,[4]]).
+% {10.00791,8}
+%
+% 3> sc_time:benchmark(fun sc_lazy:eval/2, [F,[4]]).
+% {1.0e-6,8}
+
+
+eval(Fun, Args) ->
+
+    Pid = case whereis(sc_lazy_cache) of
+
+        undefined ->
+            P = spawn(fun cache_core/0),
+            register(sc_lazy_cache, P),
+            P;
+
+        Defined ->
+            Defined
+
+    end,
+
+    Pid ! { self(), lazy_apply, Fun, Args },
+
+    receive
+        { sc_lazy_cache, Result } -> Result
+    end.
