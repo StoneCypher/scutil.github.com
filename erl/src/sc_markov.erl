4febb010ac567f5e1f0d5fa304f2eebece37a505
John Haugeland <stonecypher@gmail.com>
2009-10-30 20:01:24 +0000

    Added sc_markov:weights_for/2

--- erl/src/sc_markov.erl
+++ erl/src/sc_markov.erl
@@ -35,3 +35,13 @@ list_to_chain_pattern(List) ->
     Bases = sc_lists:key_cluster(1, [ { A,B,I } || { [A,B], I } <- sc_stats:histograph(sc_lists:all_neighbor_pairs(List, make_lists)) ]),
     RawProplist = [ { [Label], [ {[B],I} || {_A,B,I} <- InnerList ] } || { Label, InnerList } <- Bases ],
     { sc_markov_chain_pattern, dict:from_list(RawProplist) }.
+
+
+
+
+
+% @since Version 426
+
+weights_for(Item, {sc_markov_chain_pattern, Pattern}) ->
+
+    dict:fetch(Item, Pattern).
