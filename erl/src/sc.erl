583f032970520e26cc46e79ea1c1094efd03bda1
John Haugeland <stonecypher@gmail.com>
2011-03-22 00:41:22 +0000

    doc update, sc_rank is now part of sc

--- erl/src/sc.erl
+++ erl/src/sc.erl
@@ -4182,7 +4182,7 @@ pearson_correlation(List1, List2) when is_list(List1), is_list(List2) ->
 %% @todo comeback make a simple/2 which takes a sorting predicate
 %% @spec simple_ranking(Values::numericlist()) -> rankinglist()
 
-%% @doc {@section Statistics} Returns a ranked ordering of the list without tie rankings.  ```1> sc_rank:simple([10,90,20,80,30,70,40,60,50]).
+%% @doc <span style="color:orange;font-style:italic">Untested</span> Returns a ranked ordering of the list without tie rankings.  ```1> sc_rank:simple([10,90,20,80,30,70,40,60,50]).
 %% [{1,90}, {2,80}, {3,70}, {4,60}, {5,50}, {6,40}, {7,30}, {8,20}, {9,10}]
 %%
 %% 2> sc_rank:simple([10,10,10,10]).
@@ -4203,7 +4203,7 @@ simple_ranking(List) when is_list(List) ->
 
 %% @spec tied_ranking(Values::numericlist()) -> rankinglist()
 
-%% @doc {@section Statistics} Returns a ranked ordering of the list with tie rankings.  As such, for uniformity, all rankings are floats.  Ties are represented as the centers of ranges. ```1> scutil:tied([10,90,20,80,30,70,40,60,50]).
+%% @doc <span style="color:orange;font-style:italic">Untested</span> Returns a ranked ordering of the list with tie rankings.  As such, for uniformity, all rankings are floats.  Ties are represented as the centers of ranges. ```1> scutil:tied([10,90,20,80,30,70,40,60,50]).
 %% [{1.0,90}, {2.0,80}, {3.0,70}, {4.0,60}, {5.0,50}, {6.0,40}, {7.0,30}, {8.0,20}, {9.0,10}]
 %%
 %% 2> scutil:tied([100,200,200,300]).
@@ -4266,7 +4266,7 @@ tied_rank_worker([Item|Remainder], Work, PrevValue) ->
 
 %% @spec tied_ordered_ranking(Values::numericlist()) -> rankinglist()
 
-%% @doc {@section Statistics} Returns a tied ranked ordering of the list, ordered according to the input ordering rather than the sorted ordering.  As with {@link tied/1}, all rankings are floats, and ties are represented as the centers of ranges. ```1> scutil:ordered([10,90,20,80,30,70,40,60,50]).
+%% @doc <span style="color:orange;font-style:italic">Untested</span> Returns a tied ranked ordering of the list, ordered according to the input ordering rather than the sorted ordering.  As with {@link tied/1}, all rankings are floats, and ties are represented as the centers of ranges. ```1> scutil:ordered([10,90,20,80,30,70,40,60,50]).
 %% [{9.0,10}, {1.0,90}, {8.0,20}, {2.0,80}, {7.0,30}, {3.0,70}, {6.0,40}, {4.0,60}, {5.0,50}]
 %%
 %% 2> scutil:ordered([100,200,200,300]).
