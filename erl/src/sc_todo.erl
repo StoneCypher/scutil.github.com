d223a007616c70ee5f22d63e517492b5374b16f3
John Haugeland <stonecypher@gmail.com>
2011-03-22 00:54:20 +0000

    Added notes for things I'll be pursuing soon in sc_todo - voting systems and criteria; decision and analysis methods

--- erl/src/sc_todo.erl
+++ erl/src/sc_todo.erl
@@ -13,3 +13,37 @@ from sc_lists
 % todo invert this so that it returns {currentcount, fun, result} so that it can be continued
 % generate(0, _) -> [];
 % generate(N, Fun) when is_integer(N) andalso N > 0 andalso is_function(Fun) -> [Fun()] ++ generate(N-1,Fun).
+
+http://en.wikipedia.org/wiki/Naive_Bayes_classifier
+http://en.wikipedia.org/wiki/Boosted_trees#Gradient_tree_boosting
+http://en.wikipedia.org/wiki/Decision_tree
+http://en.wikipedia.org/wiki/Classification_and_regression_tree
+http://en.wikipedia.org/wiki/Random_forests
+http://www.ibm.com/developerworks/web/library/wa-bayes1/
+
+http://en.wikipedia.org/wiki/Approval_voting
+http://en.wikipedia.org/wiki/Range_voting
+http://en.wikipedia.org/wiki/Plurality_voting_system
+http://en.wikipedia.org/wiki/Burr_dilemma
+http://en.wikipedia.org/wiki/Copeland%27s_method
+http://en.wikipedia.org/wiki/Kemeny-Young_method
+http://en.wikipedia.org/wiki/Preferential_ballot
+http://en.wikipedia.org/wiki/Single_transferable_vote
+http://en.wikipedia.org/wiki/Instant-runoff_voting
+http://en.wikipedia.org/wiki/Minimax_Condorcet
+http://en.wikipedia.org/wiki/Nanson%27s_method
+http://en.wikipedia.org/wiki/Ranked_pairs
+http://en.wikipedia.org/wiki/Schulze_method
+http://en.wikipedia.org/wiki/Borda_count
+http://en.wikipedia.org/wiki/Plurality_voting
+http://en.wikipedia.org/wiki/Bucklin_voting
+http://en.wikipedia.org/wiki/Preferential_voting
+http://en.wikipedia.org/wiki/Category:Voting_system_criteria
+http://en.wikipedia.org/wiki/Coombs%27_method
+http://en.wikipedia.org/wiki/Contingent_vote
+http://en.wikipedia.org/wiki/Independence_of_irrelevant_alternatives
+http://en.wikipedia.org/wiki/Majority_criterion
+http://en.wikipedia.org/wiki/Arrow%27s_impossibility_theorem
+http://en.wikipedia.org/wiki/Later-no-harm_criterion
+http://en.wikipedia.org/wiki/Condorcet_winner
+http://en.wikipedia.org/wiki/Condorcet_loser
