293987050f012f3c28195d47ff89ea95270f5387
John Haugeland <stonecypher@gmail.com>
2011-03-28 00:08:20 +0000

    start_register_if_not_running/3, /4, /5

--- erl/src/sc.erl
+++ erl/src/sc.erl
@@ -107,6 +107,10 @@
     record_member/2,
     get_linked_processes/0,
 
+    start_register_if_not_running/3,
+      start_register_if_not_running/4,
+      start_register_if_not_running/5,
+
     tuple_sum/1,
       tuple_sort/1,
       tuple_member/2,
@@ -5999,7 +6003,7 @@ record_member(E, R) ->
 
 
 
-%% @doc <span style="color:orange;font-style:italic">Untested</span> 
+%% @doc <span style="color:orange;font-style:italic">Untested</span>
 
 %% @since Version 617
 
@@ -6012,3 +6016,82 @@ get_linked_processes() ->
           ],
 
     U.
+
+
+
+
+
+%% @equiv start_register_if_not_running(node(), Name, Module, Function, [])
+
+%% @doc <span style="color:orange;font-style:italic">Untested</span>
+
+%% @since Version 618
+
+start_register_if_not_running(Name, Module, Function) ->
+
+    start_register_if_not_running(node(), Name, Module, Function, []).
+
+
+
+
+
+%% @doc <span style="color:orange;font-style:italic">Untested</span>
+
+%% @equiv start_register_if_not_running(node(), Name, Module, Function, Args)
+
+start_register_if_not_running(Name, Module, Function, Args) ->
+
+    start_register_if_not_running(node(), Name, Module, Function, Args).
+
+
+
+
+
+%% @spec start_register_if_not_running(Node::atom(), Name::atom(), Module::atom(), Function::atom(), Args::list()) -> pid() | ok
+
+%% @doc <span style="color:orange;font-style:italic">Untested</span> Check whether a process is registered locally, and if not, spawn it with a give function and arguments.  ```1> whereis(test).
+%% undefined
+%%
+%% 2> scutil:start_register_if_not_running(node(), test, scutil, wait_until_terminate, []).
+%% ok
+%%
+%% 3> whereis(test).
+%% <0.726.0>
+%%
+%% 4> test ! terminate.
+%% terminate
+%%
+%% 5> whereis(test).
+%% undefined
+%%
+%% 6> scutil:start_register_if_not_running(node(), test, scutil, wait_until_terminate, []).
+%% true
+%%
+%% 7> whereis(test).
+%% <0.731.0>
+%%
+%% 8> scutil:start_register_if_not_running(node(), test, scutil, wait_until_terminate, []).
+%% ok
+%%
+%% 9> whereis(test).
+%% <0.731.0>'''
+
+%% @since Version 618
+
+start_register_if_not_running(Node, Name, Module, Function, Args)
+
+    when is_atom(Name),
+         is_atom(Module),
+         is_atom(Function),
+         is_list(Args)    ->
+
+    case whereis(Name) of
+
+        undefined ->
+            register(Name, spawn(Node, Module, Function, Args)),
+            ok;
+
+        _ ->
+            ok
+
+    end.
