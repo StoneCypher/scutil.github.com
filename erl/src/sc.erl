a19848acb60f9f4bd1e25ddf126c84923f231439
John Haugeland <stonecypher@gmail.com>
2011-10-29 18:32:36 -0400

    type_of/1

--- erl/src/sc.erl
+++ erl/src/sc.erl
@@ -162,6 +162,7 @@
     to_lines/1,
     isolate_waveform/1,
     unit_scale/1,
+    type_of/1,
 
     regex_matches/2,
       regex_matches/3,
@@ -8398,9 +8399,9 @@ regex_matches(String, Reg, TrimFront, TrimLength) ->
 
 
 
-%% @spec matches(String::string(), Reg::string(), { TrimFront::integer(), TrimLength::integer() }) -> list() | { error, E }
+%% @spec regex_matches(String::string(), Reg::string(), { TrimFront::integer(), TrimLength::integer() }) -> list() | { error, E }
 
-%% @doc {@section Regex} Take a string and a regular expression (and optionally an offset and length to trim to in each result), and return a list of all matches.  For a trim length of {A,B}, the first A and last B characters of each result will be removed.```1> sc:regex_matches("0j2  4g5  8t9", "[0-9](.)[0-9]").
+%% @doc Take a string and a regular expression (and optionally an offset and length to trim to in each result), and return a list of all matches.  For a trim length of {A,B}, the first A and last B characters of each result will be removed.```1> sc:regex_matches("0j2  4g5  8t9", "[0-9](.)[0-9]").
 %% ["0j2","4g5","8t9"]
 %%
 %% 2> sc:regex_matches("0j2  4g5  8t9", "[0-9](.)[0-9]", {1,1}).
@@ -8424,3 +8425,34 @@ regex_matches(String, Reg, {TrimFront, TrimLength}) ->
             { error, E }
 
     end.
+
+
+
+
+
+%% @type typelabel() = [ integer | float | list | tuple | binary | bitstring | boolean | function | pid | port | reference | atom | unknown ].  Used by type_of(), this is just any single item from the list of erlang's primitive types, or the atom <tt>unknown</tt>.
+
+%% @spec type_of(Argument::any()) -> typelabel()
+
+%% @doc {@section Utility} Fetch the type of the argument.  Valid for any term.  Fails before erlang 12, due to use of `is_bitstring()' . ```1> scutil:type_of(1).
+%% integer
+%%
+%% 2> scutil:type_of({hello,world}).
+%% tuple'''
+
+%% @since Version 722
+
+type_of(X) when is_integer(X)   -> integer;
+type_of(X) when is_float(X)     -> float;
+type_of(X) when is_list(X)      -> list;
+type_of(X) when is_tuple(X)     -> tuple;
+type_of(X) when is_binary(X)    -> binary;
+type_of(X) when is_bitstring(X) -> bitstring;  % will fail before erlang 12
+type_of(X) when is_boolean(X)   -> boolean;
+type_of(X) when is_function(X)  -> function;
+type_of(X) when is_pid(X)       -> pid;
+type_of(X) when is_port(X)      -> port;
+type_of(X) when is_reference(X) -> reference;
+type_of(X) when is_atom(X)      -> atom;
+
+type_of(_X)                     -> unknown.
