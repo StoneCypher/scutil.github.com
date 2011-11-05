d73090ff6893ad7d36fd18beac430f60f15623ac
John Haugeland <stonecypher@gmail.com>
2011-11-05 14:20:58 -0400

    colored version line in tick.sh, attempt 2

--- tick.sh
+++ tick.sh
@@ -5,7 +5,10 @@ next=`expr $ver + 1`
 `echo $next > version.counter`
 gver=$majorver.$minorver.$next
 
-echo "\e[1;34mUpdating $majorver.$minorver.$ver to $next (git $gver)\e[0m"
+blue='\e[1;34m'
+gray='\e[0m'
+
+echo "${blue}Updating $majorver.$minorver.$ver to $next (git $gver)$gray"
 
 `git tag -a $gver -m 'Version $gver'`
 echo Created `git describe --tags --long`
