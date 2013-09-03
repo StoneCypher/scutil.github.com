2d167c64ed1f069900295ccc668b3b7a4b1972ec
John Haugeland <stonecypher@gmail.com>
2013-09-03 02:08:20 -0700

    Added restaurant example htstub server

---
+++ src/htstub_servers/restaurant.erl
@@ -0,0 +1,41 @@
+
+-module(restaurant).
+
+
+
+
+
+-export([
+
+    server/1
+
+]).
+
+
+
+
+
+restaurants() ->
+
+    [
+        <<"Tony Baloneys">>,
+        <<"Tartine">>,
+        <<"Foreign Cinema">>,
+        <<"Super Duper">>,
+        <<"Roam">>,
+        <<"Espetus">>,
+        <<"SoMa StrEat Food">>,
+        <<"Pizza Hut">>
+    ].
+
+
+
+
+
+server(_) -> 
+
+    << 
+        <<"<!doctype html><html><body>">> / binary, 
+        (sc:random_from( restaurants() )) / binary, 
+        <<"</body></html>">>              / binary
+    >>.
